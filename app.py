d57d40a7fafc0786e28d3a655f051544e51aba61
Kranti <62793680+kranti-experiments@users.noreply.github.com>
2021-08-19 12:17:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,25 @@
-# -*- coding: utf-8 -*-
-
 from flask import Flask, render_template
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from forms import UserForm
 
 #Create a flask instance
 app = Flask(__name__)
 # Create Secret Key
 app.config['SECRET_KEY'] = 'tsai'
 
-# Create a Form Class
-class userform(FlaskForm):
-    name = StringField("What's Your Name", validators=[DataRequired()])
-    submit = SubmitField("Submit")
 
 @app.route('/', methods=['GET', 'POST'])
 def homepage():
     name = None
-    form = userform()
+    form = UserForm()
 
     #Validate Form
     if form.validate_on_submit():
         name = form.name.data
         form.name.data = ''
 
-    return render_template("name.html",
+    return render_template("user.html",
                            name = name,
                            form = form)
 
 if __name__ == '__main__':
-    app.run(host="127.0.0.1", port=8080, debug=True)
+    app.run(debug=True)
